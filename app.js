39b68e16aa452e11888fa7260d9e6d8cad76208f
Rio <Nesta.loisy@gmail.com>
2021-10-30 15:40:40 +0200

    js filters

--- app.js
+++ app.js
@@ -50,6 +50,49 @@ const products = [
     },
 ]
 
+function newItem(index) {
+    product = products[index]
+
+    // Container item
+    itemContainer = document.createElement('div')
+    itemContainer.setAttribute("id", "item" + String(index))
+    itemContainer.setAttribute("class", "item")
+    document.getElementById("gallery").appendChild(itemContainer)
+
+    // Image
+    img = document.createElement("img")
+    img.setAttribute("class", "imgProduct")
+    img.setAttribute("src", `res/${product.category}/${(product.name.replace(/\s+/g, '')).toLowerCase() + product.color + String(product.storage)}.png`)
+    document.getElementById("item" + String(index)).appendChild(img) 
+    
+    // Container Infos
+    itemContainerInfos = document.createElement('div')
+    itemContainerInfos.setAttribute("id", "desc" + String(index))
+    itemContainerInfos.setAttribute("class", "desc")
+    document.getElementById("item" + String(index)).appendChild(itemContainerInfos)
+
+    // Product title
+    productTitle = document.createElement('h3')
+    productTitle.setAttribute("id", "nameProduct" + String(index))
+    productTitle.setAttribute("class", "nameProduct")
+    document.getElementById("desc" + String(index)).appendChild(productTitle)  
+    document.getElementById("nameProduct" + String(index)).innerText = product.name
+
+    // Product desc
+    productDesc = document.createElement('p')
+    productDesc.setAttribute("id", "descProduct" + String(index))
+    productDesc.setAttribute("class", "descProduct")
+    document.getElementById("desc" + String(index)).appendChild(productDesc) 
+    document.getElementById("descProduct" + String(index)).innerText = product.color + " - " + product.storage + " Go"
+    
+
+    // Product price
+    productPrice = document.createElement('p')
+    productPrice.setAttribute("id", "price" + String(index))
+    productPrice.setAttribute("class", "price")
+    document.getElementById("desc" + String(index)).appendChild(productPrice) 
+    document.getElementById("price" + String(index)).innerText = product.price + " €"
+}
 function initGallery() {
 
     nbItems = products.length
@@ -57,49 +100,59 @@ function initGallery() {
     document.getElementById('gallery').innerHTML = ""
     while (item != nbItems) {
         
-        product = products[item]
-
-        // Container item
-        itemContainer = document.createElement('div')
-        itemContainer.setAttribute("id", "item" + String(item))
-        itemContainer.setAttribute("class", "item")
-        document.getElementById("gallery").appendChild(itemContainer)
-
-        // Image
-        img = document.createElement("img")
-        img.setAttribute("class", "imgProduct")
-        img.setAttribute("src", `res/${product.category}/${(product.name.replace(/\s+/g, '')).toLowerCase() + product.color + String(product.storage)}.png`)
-        document.getElementById("item" + String(item)).appendChild(img) 
-        
-        // Container Infos
-        itemContainerInfos = document.createElement('div')
-        itemContainerInfos.setAttribute("id", "desc" + String(item))
-        itemContainerInfos.setAttribute("class", "desc")
-        document.getElementById("item" + String(item)).appendChild(itemContainerInfos)
-
-        // Product title
-        productTitle = document.createElement('h3')
-        productTitle.setAttribute("id", "nameProduct" + String(item))
-        productTitle.setAttribute("class", "nameProduct")
-        document.getElementById("desc" + String(item)).appendChild(productTitle)  
-        document.getElementById("nameProduct" + String(item)).innerText = product.name
-
-        // Product desc
-        productDesc = document.createElement('p')
-        productDesc.setAttribute("id", "descProduct" + String(item))
-        productDesc.setAttribute("class", "descProduct")
-        document.getElementById("desc" + String(item)).appendChild(productDesc) 
-        document.getElementById("descProduct" + String(item)).innerText = product.color + " - " + product.storage + " Go"
-        
-
-        // Product price
-        productPrice = document.createElement('p')
-        productPrice.setAttribute("id", "price" + String(item))
-        productPrice.setAttribute("class", "price")
-        document.getElementById("desc" + String(item)).appendChild(productPrice) 
-        document.getElementById("price" + String(item)).innerText = product.price + " €"
+        newItem(item)
 
         item += 1
 
     }
 }
+
+filterColor = []
+
+function addFilter(nameFilter, valueFilter) {
+    if (nameFilter == "color") {
+        colorFilter(valueFilter)
+    }
+    if (nameFilter == "price") {
+        priceFilter(valueFilter)
+    }
+}
+
+function colorFilter() {
+    nbItems = products.length
+    item = 0
+    document.getElementById('gallery').innerHTML = ""
+    while (item != nbItems) {
+        filterColor.forEach(colorId => {
+            if (products[item].color == colorId) {
+                newItem(item)
+            }
+        });
+        item += 1
+    }
+}
+
+document.querySelectorAll('.colorFilter').forEach(function(el){
+    el.addEventListener('click', function() {
+        input = document.getElementById((this.id).toLowerCase())
+        label = document.getElementById("color" + this.id)
+        if (input.checked == false) {
+            filterColor.push(this.id)
+            colorFilter()
+            label.classList.add("selectedFilter")
+            input.checked = true
+        }else {
+            indexOfColor = filterColor.indexOf(this.id)
+            filterColor.splice(indexOfColor, 1)
+            label.classList.remove("selectedFilter")
+            input.checked = false
+            if (filterColor.length == 0) {
+                initGallery()
+            }else {
+                filterColor.forEach(color => {
+                    colorFilter(color)
+                });
+            }
+        }
+    });
+});
